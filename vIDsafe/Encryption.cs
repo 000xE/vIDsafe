bc7eb32e6af170d7a2dca859714e0e8bb130bad2
outerme <41651226+outerme@users.noreply.github.com>
2021-01-07 23:01:49 +0000

    Made the IV for AES auto generate and embedded it onto the ciphertext

--- vIDsafe/Encryption.cs
+++ vIDsafe/Encryption.cs
@@ -1,27 +1,22 @@
-﻿using SimpleCrypto;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace vIDsafe
 {
     class Encryption
     {
-
-        public static ICryptoService cryptoService = new PBKDF2();
         private const int HASH_ITERATIONS = 100000; //Work factor, higher = longer
-
-        private static string IV = "fu1$c!j2d8limk6x";
-
         private const int HASH_SIZE = 32;
 
-        //https://shawnmclean.com/simplecrypto-net-a-pbkdf2-hashing-wrapper-for-net-framework/
         public static byte[] hashPassword(string newPassword, string salt)
         {
             //a new password hash is generated from a generated salt with the passed settings
+            //https://shawnmclean.com/simplecrypto-net-a-pbkdf2-hashing-wrapper-for-net-framework/
             //return cryptoService.Compute(newPassword, HASH_ITERATIONS + "." + salt);
 
             byte[] convertedSalt = ASCIIEncoding.ASCII.GetBytes(salt);
@@ -31,63 +26,74 @@ namespace vIDsafe
             return pbkdf2.GetBytes(HASH_SIZE);
         }
 
-        //https://shawnmclean.com/simplecrypto-net-a-pbkdf2-hashing-wrapper-for-net-framework/
-        public static bool validatePassword(string password, string hashedPassword, string salt)
-        {
-            //hash the password with the saved salt for that user
-            string hashed = cryptoService.Compute(password, salt);
-            //return true if both hashes are the same
-            return hashed == hashedPassword;
-        }
 
         public static string aesEncrypt(string plainText, byte[] key)
         {
             byte[] textBytes = ASCIIEncoding.ASCII.GetBytes(plainText);
 
-            AesCryptoServiceProvider AES = new AesCryptoServiceProvider
+            using (AesCryptoServiceProvider AES = new AesCryptoServiceProvider())
             {
-                BlockSize = 128,
-                KeySize = 256,
-                Key = key,
-                IV = ASCIIEncoding.ASCII.GetBytes(IV),
-                Padding = PaddingMode.PKCS7,
-                Mode = CipherMode.CBC
+                AES.BlockSize = 128;
+                AES.KeySize = 256;
+                AES.Key = key;
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CBC;
+
+                //https://stackoverflow.com/q/8041451
+                using (var encryptor = AES.CreateEncryptor(AES.Key, AES.IV))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        ms.Write(AES.IV, 0, 16);
+                        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            cs.Write(textBytes, 0, textBytes.Length);
+                            cs.FlushFinalBlock();
+                        }
+
+                        Console.WriteLine(Convert.ToBase64String(AES.IV));
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
             };
-
-            ICryptoTransform Encryptor = AES.CreateEncryptor(AES.Key, AES.IV);
-
-            byte[] encryptedText = Encryptor.TransformFinalBlock(textBytes, 0, textBytes.Length);
-            Encryptor.Dispose();
-
-            return Convert.ToBase64String(encryptedText);
         }
 
 
         public static string aesDecrypt(string encryptedText, byte[] key)
         {
             byte[] textBytes = Convert.FromBase64String(encryptedText);
-            AesCryptoServiceProvider AES = new AesCryptoServiceProvider
+            using (AesCryptoServiceProvider AES = new AesCryptoServiceProvider())
             {
-                BlockSize = 128,
-                KeySize = 256,
-                Key = key,
-                IV = ASCIIEncoding.ASCII.GetBytes(IV),
-                Padding = PaddingMode.PKCS7,
-                Mode = CipherMode.CBC
+                AES.BlockSize = 128;
+                AES.KeySize = 256;
+                AES.Key = key;
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CBC;
+
+                //https://stackoverflow.com/q/8041451
+                using (var ms = new MemoryStream(textBytes))
+                {
+                    byte[] buffer = new byte[16];
+                    ms.Read(buffer, 0, 16);
+                    AES.IV = buffer;
+                    using (var decryptor = AES.CreateDecryptor(AES.Key, AES.IV))
+                    {
+                        using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            try
+                            {
+                                byte[] decrypted = new byte[textBytes.Length];
+                                var byteCount = cs.Read(decrypted, 0, textBytes.Length);
+                                return Encoding.UTF8.GetString(decrypted, 0, byteCount);
+                            }
+                            catch (CryptographicException)
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                }
             };
-
-            ICryptoTransform Encryptor = AES.CreateDecryptor(AES.Key, AES.IV);
-            try
-            {
-                byte[] decryptedText = Encryptor.TransformFinalBlock(textBytes, 0, textBytes.Length);
-                Encryptor.Dispose();
-
-                return ASCIIEncoding.ASCII.GetString(decryptedText);
-            }
-            catch (CryptographicException)
-            {
-                return null;
-            }
         }
     }
 }
