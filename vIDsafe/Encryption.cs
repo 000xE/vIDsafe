e5fd04ffd2860cbafa27eb55f5f169c7659656a4
outerme <41651226+outerme@users.noreply.github.com>
2021-01-08 00:06:56 +0000

    Expanded the area of validation in the decryption

--- vIDsafe/Encryption.cs
+++ vIDsafe/Encryption.cs
@@ -55,7 +55,7 @@ namespace vIDsafe
                             cs.FlushFinalBlock();
                         }
 
-                        Console.WriteLine(Convert.ToBase64String(AES.IV));
+
                         return Convert.ToBase64String(ms.ToArray());
                     }
                 }
@@ -75,13 +75,14 @@ namespace vIDsafe
                 AES.Mode = CipherMode.CBC;
 
                 //https://stackoverflow.com/q/8041451
-                using (var ms = new MemoryStream(textBytes))
+                try
                 {
-                    byte[] buffer = new byte[ivSize];
-                    ms.Read(buffer, 0, ivSize);
-                    AES.IV = buffer;
-                    try
+                    using (var ms = new MemoryStream(textBytes))
                     {
+                        byte[] buffer = new byte[ivSize];
+                        ms.Read(buffer, 0, ivSize);
+                        AES.IV = buffer;
+
                         using (var decryptor = AES.CreateDecryptor(AES.Key, AES.IV))
                         {
                             using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
@@ -92,10 +93,10 @@ namespace vIDsafe
                             }
                         }
                     }
-                    catch (CryptographicException)
-                    {
-                        return null;
-                    }
+                }
+                catch (CryptographicException)
+                {
+                    return null;
                 }
             };
         }
