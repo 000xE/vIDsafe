de486934d4f1a316aa87ae494f64951e2b91e5a2
outerme <41651226+outerme@users.noreply.github.com>
2021-02-24 03:44:55 +0000

    Separated the porting methods by format

--- vIDsafe/Vault related/Porting.cs
+++ vIDsafe/Vault related/Porting.cs
@@ -21,7 +21,7 @@ namespace vIDsafe
         /// Tries to serialise a vault to a format
         /// </summary>
         /// <returns>
-        /// True if vault is serialised, false if not
+        /// Serialised vault, null if not
         /// </returns>
         protected string TrySerializeVault(VaultFormat format, Vault vault, string password)
         {
@@ -32,33 +32,13 @@ namespace vIDsafe
                 switch (format)
                 {
                     case VaultFormat.CSV:
-                        using (StringWriter stringWriter = new StringWriter())
-                        using (CsvWriter csvWriter = new CsvWriter(stringWriter, CultureInfo.InvariantCulture))
-                        {
-                            List<Identity> identities = vault.Identities.Values.ToList();
-
-                            csvWriter.WriteHeader<Identity>();
-                            csvWriter.WriteHeader<Credential>();
-                            csvWriter.NextRecord();
-
-                            foreach (Identity identity in identities)
-                            {
-                                foreach (Credential credential in identity.Credentials.Values.ToList())
-                                {
-                                    csvWriter.WriteRecord(identity);
-                                    csvWriter.WriteRecord(credential);
-                                    csvWriter.NextRecord();
-                                }
-                            }
-
-                            serialisedVault = stringWriter.ToString();
-                        }
+                        serialisedVault = SerializeCSV(vault);
                         break;
                     case VaultFormat.JSON:
-                        serialisedVault = JsonConvert.SerializeObject(vault, Formatting.Indented);
+                        serialisedVault = SerializeJSON(vault);
                         break;
                     case VaultFormat.Encrypted:
-                        serialisedVault = EncryptVault(vault, password);
+                        serialisedVault = SerializeEncrypted(vault, password);
                         break;
                 }
 
@@ -75,9 +55,9 @@ namespace vIDsafe
         /// Tries to deserialise a vault from a format
         /// </summary>
         /// <returns>
-        /// True if vault is deserialise, false if not
+        /// Deserialised vault, null if not
         /// </returns>
-        protected Vault TryDeserializeObject(VaultFormat format, string serialisedVault, string password)
+        protected Vault TryDeserializeObject(VaultFormat format, string vault, string password)
         {
             Vault deserialisedVault = new Vault();
 
@@ -86,29 +66,13 @@ namespace vIDsafe
                 switch (format)
                 {
                     case VaultFormat.CSV:
-                        using (StringReader stringReader = new StringReader(serialisedVault))
-                        using (CsvReader csvReader = new CsvReader(stringReader, CultureInfo.InvariantCulture))
-                        {
-                            csvReader.Read();
-                            csvReader.ReadHeader();
-
-                            while (csvReader.Read())
-                            {
-                                Identity identity = csvReader.GetRecord<Identity>();
-
-                                identity = deserialisedVault.FindOrCreateIdentity(identity.Name, identity.Email, identity.Usage);
-
-                                Credential credential = csvReader.GetRecord<Credential>();
-
-                                identity.FindOrCreateCredential(credential.CredentialID, credential.Username, credential.Password, credential.URL, credential.Notes);
-                            }
-                        }
+                        deserialisedVault = DeserializeCSV(vault);
                         break;
                     case VaultFormat.JSON:
-                        deserialisedVault = JsonConvert.DeserializeObject<Vault>(serialisedVault);
+                        deserialisedVault = DeserializeJSON(vault);
                         break;
                     case VaultFormat.Encrypted:
-                        deserialisedVault = DecryptVault(serialisedVault, password);
+                        deserialisedVault = DeserializeEncrypted(vault, password);
                         break;
                 }
 
@@ -119,6 +83,124 @@ namespace vIDsafe
                 Console.WriteLine(e);
                 return null;
             }
+        }
+
+        /// <summary>
+        /// Tries to serialise a vault to CSV
+        /// </summary>
+        /// <returns>
+        /// Serialised vault, empty vault if not
+        /// </returns>
+        private string SerializeCSV(Vault vault)
+        {
+            string serialisedVault = "";
+
+            using (StringWriter stringWriter = new StringWriter())
+            using (CsvWriter csvWriter = new CsvWriter(stringWriter, CultureInfo.InvariantCulture))
+            {
+                List<Identity> identities = vault.Identities.Values.ToList();
+
+                csvWriter.WriteHeader<Identity>();
+                csvWriter.WriteHeader<Credential>();
+                csvWriter.NextRecord();
+
+                foreach (Identity identity in identities)
+                {
+                    foreach (Credential credential in identity.Credentials.Values.ToList())
+                    {
+                        csvWriter.WriteRecord(identity);
+                        csvWriter.WriteRecord(credential);
+                        csvWriter.NextRecord();
+                    }
+                }
+
+                serialisedVault = stringWriter.ToString();
+            }
+
+            return serialisedVault;
+        }
+
+        /// <summary>
+        /// Tries to serialise a vault to JSON
+        /// </summary>
+        /// <returns>
+        /// Serialised vault, empty vault if not
+        /// </returns>
+        private string SerializeJSON(Vault vault)
+        {
+            string serialisedVault = JsonConvert.SerializeObject(vault, Formatting.Indented);
+
+            return serialisedVault;
+        }
+
+        /// <summary>
+        /// Tries to serialise a vault to encrypted
+        /// </summary>
+        /// <returns>
+        /// Serialised vault, empty vault if not
+        /// </returns>
+        private string SerializeEncrypted(Vault vault, string password)
+        {
+            string serialisedVault = EncryptVault(vault, password);
+
+            return serialisedVault;
+        }
+
+        /// <summary>
+        /// Tries to deserialise a vault from CSV
+        /// </summary>
+        /// <returns>
+        /// Deserialised vault, empty vault if not
+        /// </returns>
+        private Vault DeserializeCSV(string vault)
+        {
+            Vault deserialisedVault = new Vault();
+
+            using (StringReader stringReader = new StringReader(vault))
+            using (CsvReader csvReader = new CsvReader(stringReader, CultureInfo.InvariantCulture))
+            {
+                csvReader.Read();
+                csvReader.ReadHeader();
+
+                while (csvReader.Read())
+                {
+                    Identity identity = csvReader.GetRecord<Identity>();
+
+                    identity = deserialisedVault.FindOrCreateIdentity(identity.Name, identity.Email, identity.Usage);
+
+                    Credential credential = csvReader.GetRecord<Credential>();
+
+                    identity.FindOrCreateCredential(credential.CredentialID, credential.Username, credential.Password, credential.URL, credential.Notes);
+                }
+            }
+
+            return deserialisedVault;
+        }
+
+        /// <summary>
+        /// Tries to deserialise a vault from JSON
+        /// </summary>
+        /// <returns>
+        /// Deserialised vault, empty vault if not
+        /// </returns>
+        private Vault DeserializeJSON(string vault)
+        {
+            Vault deserialisedVault = JsonConvert.DeserializeObject<Vault>(vault);
+
+            return deserialisedVault;
+        }
+
+        /// <summary>
+        /// Tries to deserialise a vault from encrypted
+        /// </summary>
+        /// <returns>
+        /// Deserialised vault, empty vault if not
+        /// </returns>
+        private Vault DeserializeEncrypted(string vault, string password)
+        {
+            Vault deserialisedVault = DecryptVault(vault, password);
+
+            return deserialisedVault;
         }
 
         /// <summary>
